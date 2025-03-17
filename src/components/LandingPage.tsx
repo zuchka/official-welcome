92f42752218413fa9a728218badd380a0628c025
zuchka <zuchka@protonmail.ch>
2025-03-16 19:26:39 -0700

    firts commit

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,7 +3,7 @@ import { EmailForm } from "./EmailForm";
 export const LandingPage = () => {
   return (
     <div className="min-h-screen w-full flex flex-col items-center px-5 py-20 bg-gradient-to-b from-gray-100 via-gray-200 to-gray-400">
-      <h1 className="font-['Cormorant_Garamond'] text-[32px] md:text-[60px] lg:text-[90px] font-normal text-[#464646] tracking-[12px] md:tracking-[25px] lg:tracking-[35px] text-center mt-[20px] max-w-full leading-tight">
+      <h1 className="font-['Cormorant_Garamond'] text-[32px] md:text-[60px] lg:text-[90px] font-normal text-[#464646] tracking-[12px] md:tracking-[25px] lg:tracking-[35px] text-center mt-[5px] md:mt-[20px] max-w-full leading-tight">
         OFFICIAL WELCOME
       </h1>
       <h2 className="font-['Cormorant_Garamond'] text-[36px] md:text-[70px] lg:text-[100px] font-normal text-[#5B5B5B] tracking-[16px] md:tracking-[32px] lg:tracking-[48px] text-center mt-[60px] md:mt-[90px] lg:mt-[120px] max-w-full leading-tight">
